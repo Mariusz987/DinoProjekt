68478dadd7eeb8eb1a90cf9c8e09f2a748f4ae5b
Mariusz987 <mariuszmotyka7@gmail.com>
2020-07-15 16:39:37 +0200

    Test1

---
+++ DinoTest/DinoTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DinoProject;
+
+
+
+namespace DinoTest
+{
+    [TestClass]
+    public class DinozaurTest
+    {
+        [TestMethod]
+        public void dobreZnaki()
+        {
+            //Arrange
+            string wartosc = "2o1";
+            string oczekiwana = "50";
+            Dinozaur dinozaur = new Dinozaur();
+
+            //Act
+            dinozaur.Dlugosc = wartosc;
+            Dinozaur.sprawdzZnaki(ref wartosc);
+            
+            
+            //Assert
+            Assert.AreEqual(oczekiwana, wartosc);
+           
+
+        }
+    }
+}
