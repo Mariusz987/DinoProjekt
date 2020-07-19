efd479d61ebf19ce3f7a5455dbfafcaf1c778c3a
Mariusz987 <mariuszmotyka7@gmail.com>
2020-07-19 12:07:58 +0200

    poprawione komentarze

--- DinoProject/MainWindow.xaml.cs
+++ DinoProject/MainWindow.xaml.cs
@@ -25,19 +25,20 @@ namespace DinoProject
             InitializeComponent();
             
         }
+       
         /// <summary>
-        /// Umożliwia przejście do okienka Miesozerne.
+        /// Metoda <c> Drapizniki_MouseRightButtonDown</c> Umożliwia przejście do Drapieżniki.
         /// </summary>
-        private void Drapizniki_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        public void Drapizniki_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.Hide();
             DrapieznikiWindow drapiezniki = new DrapieznikiWindow();
             drapiezniki.Show();
         }
-        /// <summary>
-        /// Umożliwia przejście do okienka Roslinozerne.
+		/// <summary>
+        /// Metoda <c> Roslinozerne_MouseRightButtonDown</c> Umożliwia przejście do Roślinożerne.
         /// </summary>
-        private void Roslinozerne_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        public void Roslinozerne_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.Hide();
             RoslinozerneWindow roslinozerne = new RoslinozerneWindow();
@@ -45,13 +46,23 @@ namespace DinoProject
         }
 
         /// <summary>
-        /// Umożliwia przejście do okienka StworzDino.
+        /// Metoda <c> Stworz_MouseLeftButtonDown</c> Umożliwia przejście do Tworzenie Dinozaura.
         /// </summary>
-        private void Stworz_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        public void Stworz_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.Hide();
             StworzDino stworzDino = new StworzDino();
             stworzDino.Show();
+        }
+		
+		 /// <summary>
+        /// Metoda <c> Zagraj_MouseLeftButtonDown</c> Umożliwia przejście do mini-gierki.
+        /// </summary>
+		public void Zagraj_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Hide();
+            DinoGra dinoGra = new DinoGra();
+            dinoGra.Show();
         }
     }
 }
